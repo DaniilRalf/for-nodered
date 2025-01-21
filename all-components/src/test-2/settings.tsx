5c006225cea00baec0c32cac73676c15e384a653
Daniil <daniil.tarasov@eltex.loc>
2025-01-21 16:03:56 +0700

    +

--- all-components/src/test-2/settings.tsx
+++ all-components/src/test-2/settings.tsx
@@ -1,86 +1,60 @@
-// import React from 'react';
-// import {Dropdown, MenuProps} from "antd";
-//
-// const Settings = () => {
-//
-//     const items: MenuProps['items'] = [
-//         {
-//             label: (
-//                 <div>1st menu item</div>
-//             ),
-//             key: '0',
-//         },
-//         {
-//             label: (
-//                 <div>1st menu item</div>
-//             ),
-//             key: '1',
-//         }
-//     ]
-//
-//     return (
-//         <div>
-//             <Dropdown menu={{ items }} trigger={['click']}>
-//                 <div>Click me</div>
-//             </Dropdown>
-//         </div>
-//     );
-// };
-//
-// export default Settings;
+import React, { useState } from 'react'
+import { DndProvider, useDrag, useDrop } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
+import { Dropdown } from 'antd'
 
+type DraggableMenuItemType = {
+    item: { label: string; key: string },
+    index: number,
+    moveMenuItem: (fromIndex: number, toIndex: number) => void,
+}
 
-import React, { useState } from 'react';
-import { DndProvider, useDrag, useDrop } from 'react-dnd';
-import { HTML5Backend } from 'react-dnd-html5-backend';
-import { Dropdown } from 'antd';
+const ItemType = 'MENU_ITEM'
 
-const ItemType = 'MENU_ITEM';
+const DraggableMenuItem = ({ item, index, moveMenuItem }: DraggableMenuItemType) => {
 
-const DraggableMenuItem = ({ item, index, moveMenuItem }: any) => {
     const [, ref] = useDrag({
         type: ItemType,
         item: { index },
-    });
+    })
 
     const [, drop] = useDrop({
         accept: ItemType,
-        hover: (draggedItem) => {
-            // @ts-ignore
+        hover: (draggedItem: {index: number}) => {
             if (draggedItem.index !== index) {
-                // @ts-ignore
-                moveMenuItem(draggedItem.index, index);
-                // @ts-ignore
-                draggedItem.index = index;
+                moveMenuItem(draggedItem.index, index)
+                draggedItem.index = index
             }
         },
-    });
+    })
 
     return (
         <div style={{padding: '8px', border: '1px solid #ccc', marginBottom: '4px', display: 'flex'}}>
             <div ref={(node) => ref(drop(node))}
-                 style={{marginRight: '8px', cursor: 'move'}}
+                 style={{marginRight: '20px', cursor: 'move'}}
             >drop</div>
             <div>
                 {item.label}
             </div>
         </div>
 
-    );
-};
+    )
+}
 
 const Settings = () => {
+
     const [menuItems, setMenuItems] = useState([
         {label: '1st menu item', key: '0'},
         {label: '2nd menu item', key: '1' },
-    ]);
+    ])
 
     const moveMenuItem = (fromIndex: number, toIndex: number) => {
-        const updatedItems = [...menuItems];
-        const [movedItem] = updatedItems.splice(fromIndex, 1);
-        updatedItems.splice(toIndex, 0, movedItem);
-        setMenuItems(updatedItems);
-    };
+        const updatedItems = [...menuItems]
+        const [movedItem] = updatedItems.splice(fromIndex, 1)
+        updatedItems.splice(toIndex, 0, movedItem)
+        setMenuItems(updatedItems)
+        console.log(updatedItems)
+    }
 
     const menu = (
         <ul style={{ listStyle: 'none', padding: 0 }}>
@@ -88,7 +62,7 @@ const Settings = () => {
                 <DraggableMenuItem key={item.key} index={index} item={item} moveMenuItem={moveMenuItem} />
             ))}
         </ul>
-    );
+    )
 
     return (
         <DndProvider backend={HTML5Backend}>
@@ -96,7 +70,7 @@ const Settings = () => {
                 <div style={{ padding: '8px', border: '1px solid #ccc', cursor: 'pointer' }}>Click me</div>
             </Dropdown>
         </DndProvider>
-    );
-};
+    )
+}
 
-export default Settings;
+export default Settings
